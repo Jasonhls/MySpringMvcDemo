9ca08d8f215ac795f8fb2a9b4b10c1bd2027def5
hels <1597445096@qq.com>
2021-03-30 18:29:01 +0800

    jdk和cglib动态代理生成的class反编译生成的java文件

--- src/main/java/com/cn/dynamic/jdk/UserService.java
+++ src/main/java/com/cn/dynamic/jdk/UserService.java
@@ -3,7 +3,7 @@ package com.cn.dynamic.jdk;
 /**
  * @description:
  * @author: helisen
- * @create: 2021-03-29 18:09
+ * @create: 2021-03-30 18:16
  **/
 public interface UserService {
     void sayHello(String name);
