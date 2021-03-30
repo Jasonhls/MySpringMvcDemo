e1cb1d4e347ce39128cd416585258188234afc6b
hels <1597445096@qq.com>
2021-03-30 09:30:58 +0800

    添加注释

--- src/main/java/com/cn/dynamic/DynamicTest.java
+++ src/main/java/com/cn/dynamic/DynamicTest.java
@@ -15,8 +15,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class DynamicTest {
     @Test
     public void testJdkDynamic() {
+        //第一步，会把所有的bean初始化并放入spring容器中，而beanName为userServiceImpl的bean实例为JdkDynamicAopProxy对象
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JdkConfig.class);
+        //获取的bean为JdkDynamicAopProxy对象
         UserService bean = context.getBean(UserService.class);
+        //这里的bean为代理对象，执行sayHello方法的时候，会去调用JdkDynamicAopProxy的invoke方法，因为JdkDynamicAopProxy实现了InvocationHandler
         bean.sayHello("hls");
     }
 
@@ -24,6 +27,8 @@ public class DynamicTest {
     public void testCglibDynamic() {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CglibConfig.class);
         UserServiceImpl bean = context.getBean(UserServiceImpl.class);
+        //返回的bean为UserServiceImpl$$EnhancerBySpringCGLIB$$c825bb8f@1942，
+        //会去执行CglibAopProxy的内部类DynamicAdvisedInterceptor的interceptor方法
         bean.sayHello("hls");
     }
 }
